1a45a3b7cddfd9415a2303c1c51d91fa67fa6bb8
SpheDev <81554170+sphesbu@users.noreply.github.com>
2024-01-20 21:17:12 +0200

    Add files via upload
    Creating Sql Table

---
+++ 2024-01-20/SQL_User_Table.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE C123;
+USE C123;
+CREATE TABLE users(
+   username VARCHAR,
+   firstName VARCHAR(50),
+   surname VARCHAR(50),
+   emailAddress VARCHAR(50),
+   userpassword VARCHAR(50),
+)
